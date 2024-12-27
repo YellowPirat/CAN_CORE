c02224c816e48b36423c7913d586da26ef252d65
bklaric <benjamin.klaric01@gmail.com>
2024-12-28 00:41:29 +0100

    Started working on CRC

--- src/crc/de1_crc.vhd
+++ src/crc/de1_crc.vhd
@@ -7,24 +7,28 @@ entity de1_crc is
         clk                     : in    std_logic;
         rst_n                   : in    std_logic;
 
-        sample_i                : in    std_logic;
-        stuff_bit_i             : in    std_logic;
-        rxd_sync_i              : in    std_logic;
+        sample_i                : in    std_logic; -- sample puls which signals when to sample the rxd
+        stuff_bit_i             : in    std_logic; -- bit signaling if the bit is a stuffed bit or not
+        rxd_sync_i              : in    std_logic; -- bit stream of the frame
 
-        crc_i                   : in    std_logic_vector(14 downto 0);
-        crc_valid_i             : in    std_logic;
+        crc_i                   : in    std_logic_vector(14 downto 0); -- already calculated frame CRC
+        crc_valid_i             : in    std_logic; -- valid signal for the frame CRC
 
-        enable_i                : in    std_logic;
-        reset_i                 : in    std_logic;
+        enable_i                : in    std_logic; -- enable signal for CRC calculation -> starts the CRC calculation
+        reset_i                 : in    std_logic; -- reset signal for CRC calculation -> resets the FFs in CRC calculation state
 
-        crc_error_o             : out   std_logic
-    );
+        crc_error_o             : out   std_logic -- single bit signaling if the both CRCs are the same or not: -> "1" if crc_i != calculated CRC
+    ); --                                                                                                       -> "0" if crc_i == calculated CRC
 end entity;
 
 architecture rtl of de1_crc is
 
 begin
 
-    
+    if sample_i = '1' and stuff_bit_i = '0' then
+        rxd_sync_i
+        -- code
+      end if;
+      
 
 end rtl ; -- rtl
