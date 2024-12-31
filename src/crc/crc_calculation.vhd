81d0348157b1efcc7866e1c7fa76736faeb8df79
bklaric <benjamin.klaric01@gmail.com>
2024-12-31 21:18:30 +0100

    Finished crc_calculation.vhd and modified the top level domain to correspond to the changes

--- src/crc/crc_calculation.vhd
+++ src/crc/crc_calculation.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity de1_crc is
+entity crc_calculation is
     port (
         clk                     : in    std_logic;
         rst_n                   : in    std_logic;
@@ -11,33 +11,71 @@ entity de1_crc is
         stuff_bit_i             : in    std_logic; -- bit signaling if the rxd bit is a stuffed bit or not
         rxd_sync_i              : in    std_logic; -- bit stream of the frame
 
-        crc_i                   : in    std_logic_vector(14 downto 0); -- already calculated frame CRC
+        crc_i                   : in    std_logic_vector(14 downto 0); -- CRC from the sent frame
         crc_valid_i             : in    std_logic; -- valid signal for the frame CRC, which signals that the frame CRC is valid to compare to
 
-        enable_i                : in    std_logic; -- enable signal for CRC calculation -> starts the CRC calculation
-        reset_i                 : in    std_logic; -- reset signal for CRC calculation -> resets the FFs in CRC calculation state
+        enable_i                : in    std_logic; -- enable signal for CRC calculation
+        reset_i                 : in    std_logic; -- synchronous reset signal for CRC calculation
 
-        crc_error_o             : out   std_logic -- single bit signaling if the both CRCs are the same or not: -> "1" if crc_i != calculated CRC
-    ); --                                                                                                       -> "0" if crc_i == calculated CRC
+        crc_error_o             : out   std_logic -- single bit signaling if the both CRCs are the same or not: "1" if crc_i != calculated CRC
+    ); --                                                                                                       "0" if crc_i == calculated CRC
 end entity;
 
-architecture rtl of de1_crc is
+architecture rtl of crc_calculation is
 
-    type state_t is (idle_s, calculate_crc_s, error_s);
+    type state_t is (idle_s, calculate_crc_s);
     signal current_state, new_state : state_t;
-    signal crc_value : std_logic_vector(14 downto 0);
-    constant crc_polynomial : std_logic_vector(15 downto 0) := "1100010110011001";
+    signal crc_registers, next_crc_registers : std_logic_vector(14 downto 0);
 
 begin
 
-    crc_calculation_p : process(clk, rst_n)
+    crc_calculation_p : process(current_state, rxd_sync_i, sample_i, stuff_bit_i, enable_i, reset_i, crc_registers)
     begin
-        if rst_n = '0' then
-            current_state <= idle_s;
-        elsif rising_edge(clk) then
-            if reset_i = '1' then
-                -- code
+        new_state <= current_state;
+        next_crc_registers <= crc_registers;
 
+        case current_state is
+            when idle_s =>
+                if enable_i = '1' then
+                    new_state <= calculate_crc_s;
+                else
+                    new_state <= idle_s;
+                end if;
+                if reset_i = '1' then
+                    new_state <= idle_s;
+                    next_crc_registers <= (others => '0');
+                end if;
+
+            when calculate_crc_s =>
+                if enable_i = '1' then
+                    new_state <= calculate_crc_s;
+                    if sample_i = '1' and stuff_bit_i = '0' and reset_i = '0' then
+                        next_crc_registers(0)            <= rxd_sync_i xor crc_registers(14);
+                        next_crc_registers(2 downto 1)   <= crc_registers(1 downto 0);
+                        next_crc_registers(3)            <= crc_registers(2) xor crc_registers(14);
+                        next_crc_registers(4)            <= crc_registers(3) xor crc_registers(14);
+                        next_crc_registers(6 downto 5)   <= crc_registers(5 downto 4);
+                        next_crc_registers(7)            <= crc_registers(6) xor crc_registers(14);
+                        next_crc_registers(8)            <= crc_registers(7) xor crc_registers(14);
+                        next_crc_registers(9)            <= crc_registers(8);
+                        next_crc_registers(10)           <= crc_registers(9) xor crc_registers(14);
+                        next_crc_registers(13 downto 11) <= crc_registers(12 downto 10);
+                        next_crc_registers(14)           <= crc_registers(13) xor crc_registers(14);
+                    end if;
+                else
+                        new_state <= idle_s;
+                end if;
+                if reset_i = '1' then
+                    new_state <= idle_s;
+                    next_crc_registers <= (others => '0');
+                end if;
+
+            when others =>
+                new_state <= idle_s;
+        end case;
+    end process crc_calculation_p;
+
+    crc_error_o <= '1' when (crc_registers /= crc_i) and crc_valid_i = '1' else '0';
 
     p : process(clk)
     begin 
@@ -45,10 +83,11 @@ begin
             current_state <= new_state;
             if rst_n = '0' then 
                 current_state <= idle_s;
+                crc_registers <= (others => '0');
+            else
+                crc_registers <= next_crc_registers;
             end if;
         end if;
     end process p;
-
-    crc_error_o <= '1' when crc_value = crc_i and crc_valid_i = '1' else '0';
 
 end rtl ; -- rtl
--- src/crc/de1_crc.vhd
+++ src/crc/de1_crc.vhd
@@ -7,18 +7,18 @@ entity de1_crc is
         clk                     : in    std_logic;
         rst_n                   : in    std_logic;
 
-        sample_i                : in    std_logic; -- sample pulse which signals when to sample the rxd signal
-        stuff_bit_i             : in    std_logic; -- bit signaling if the rxd bit is a stuffed bit or not
-        rxd_sync_i              : in    std_logic; -- bit stream of the frame
+        sample_i                : in    std_logic;
+        stuff_bit_i             : in    std_logic;
+        rxd_sync_i              : in    std_logic;
 
-        crc_i                   : in    std_logic_vector(14 downto 0); -- already calculated frame CRC
-        crc_valid_i             : in    std_logic; -- valid signal for the frame CRC, which signals that the frame CRC is valid to compare to
+        crc_i                   : in    std_logic_vector(14 downto 0);
+        crc_valid_i             : in    std_logic;
 
-        enable_i                : in    std_logic; -- enable signal for CRC calculation -> starts the CRC calculation
-        reset_i                 : in    std_logic; -- reset signal for CRC calculation -> resets the FFs in CRC calculation state
+        enable_i                : in    std_logic;
+        reset_i                 : in    std_logic;
 
-        crc_error_o             : out   std_logic -- single bit signaling if the both CRCs are the same or not: -> "1" if crc_i != calculated CRC
-    ); --                                                                                                       -> "0" if crc_i == calculated CRC
+        crc_error_o             : out   std_logic
+    );
 end de1_crc;
 
 architecture rtl of de1_crc is
